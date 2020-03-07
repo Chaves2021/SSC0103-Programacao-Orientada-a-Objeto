187ba2b4a962f88c97f9ac3b4213c45e364d6858
Viana2021 <caiomarcoschaves@gmail.com>
2020-03-07 01:04:52 -0300

    Exercício 6 finalizado

--- Lista1/ex6/src/Ex6.java
+++ Lista1/ex6/src/Ex6.java
@@ -4,9 +4,32 @@ public class Ex6
 {
 	public static void main(String [] args)
 	{
-		while(true)
+		double number = 1;
+		double lowestNumber = 0, highestNumber = 0;
+		int counter = 0;
+		while(number != 0)
 		{
-			System.out.println("Input a float number: ");
-		}
+			try
+			{
+				System.out.println("Input a float number: ");
+				number = EntradaTeclado.leDouble();
+				if(counter == 0)
+				{
+					lowestNumber = highestNumber = number;
+					counter++;
+				}
+				else
+				{
+					if(number < lowestNumber) lowestNumber = number;
+					else if (number >=highestNumber) highestNumber = number;
+				}
+			}
+			catch(Exception e)
+			{
+				System.out.println("Input a valid number");
+			}
+		};
+		System.out.println("Highest number: " + highestNumber);
+		System.out.println("Lowest number: " + lowestNumber);
 	}
 }
